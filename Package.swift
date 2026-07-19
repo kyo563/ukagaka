262827e6b8727ce71dde1c7bb8c8a67db1c07b03
kyo563 <219984293+kyo563@users.noreply.github.com>
2026-07-19 22:10:12 +0900

    Add Swift package manifest

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "UkagakaReproductionProject",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(
+            name: "伺か再現プロジェクト",
+            targets: ["UkagakaReproductionProject"]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "UkagakaReproductionProject",
+            resources: [
+                .process("Resources")
+            ]
+        )
+    ]
+)
